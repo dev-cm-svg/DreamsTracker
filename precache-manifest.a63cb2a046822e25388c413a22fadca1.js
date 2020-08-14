be72f6f996c58f0219af62545c235d24a3fefa54
dev-cm-svg <email@example.com>
2020-08-14 19:54:38 -0400

    Updates

---
+++ precache-manifest.a63cb2a046822e25388c413a22fadca1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95b3ba23bb5d7716919d7fe48664d9b1",
+    "url": "/dreamsTracker/index.html"
+  },
+  {
+    "revision": "62393f2057c6a1f283fc",
+    "url": "/dreamsTracker/static/css/main.f52d3968.chunk.css"
+  },
+  {
+    "revision": "da238021014503978704",
+    "url": "/dreamsTracker/static/js/2.443ddf38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dreamsTracker/static/js/2.443ddf38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62393f2057c6a1f283fc",
+    "url": "/dreamsTracker/static/js/main.5d147d80.chunk.js"
+  },
+  {
+    "revision": "36443ea5c95d888e62d2",
+    "url": "/dreamsTracker/static/js/runtime-main.73bfbedc.js"
+  }
+]);
